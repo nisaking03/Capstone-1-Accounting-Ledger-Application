8e18eb390b779eed4cfac4b9d1469be19eb689b0
nisaking03 <nisa.king03@gmail.com>
2025-10-10 14:43:29 -0400

    Still trying

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,130 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Main {
+
+    public static ArrayList<Transactions> transaction = getTransactionFromFile();
+
+    public static void main(String[]args){
+        char mainMenuOptions;
+        String mainMenu = "--------------------------------------------\n" +
+            "Home Screen\n" +
+            "D) Add Deposit\n" +
+            "P) Make Payment\n" +
+            "L) Ledger\n" +
+            "X) Exit\n" +
+            "Select where you would like to be directed.\n" +
+            "--------------------------------------------";
+
+        while(true){
+            switch (mainMenuOptions) {
+            case 'D': //go to deposit
+                break;
+            case 'P': //go make payment
+                break;
+            case 'L': //go to ledger screen
+                break;
+            case 'X': //exit
+                return;
+            default:
+                System.out.println("INVALID COMMAND!! Please select a valid option.");
+                break;
+
+        }
+
+    }
+
+}
+
+    private static void addDeposit() {
+
+    }
+    private static void makePayment() {
+
+    }
+    private static void goToLedger() {
+
+    }
+
+
+//    public static ArrayList<Transactions> getTransactionFromCode(){
+//        ArrayList<Transactions> blank = new ArrayList<>();
+//    } //unsure how to do system print
+
+    public static ArrayList<Transactions> getTransactionFromFile(){
+        ArrayList<Transactions> transaction = new ArrayList<Transactions>();
+        try{
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String lineFromString;
+
+            while((lineFromString = bufferedReader.readLine()) !=null){
+                String[] part = lineFromString.split("\\|");
+                String date = part[0];
+                String time = part[1];
+                String description = part[2];
+                String vendor = part[3];
+                double amount = Double.parseDouble(part[4]);
+
+                Transactions t = new Transactions(date, time, description, vendor, amount);
+                transaction.add(t);
+            }//date|time|description|vendor|amount
+
+        }
+        catch (Exception e){
+            System.out.println("There was a problem, try again!");
+        }
+
+        return transaction;
+
+    }
+
+
+
+
+
+
+    //todo I will need at ("Home Screen")
+    //(May be a Class/Method) //Will need Scanner
+
+    /*Will contain:todo D) Add Deposit - prompt user for the deposit
+                    information and save it to the csv file (" transactions.csv ")*/
+    /*Will contain: todo P) Make Payment (Debit) - prompt user for the debit
+                     information and save it to the csv file (" transactions.csv ")*/
+    /*Will contain: todo L) Ledger - display the ledger screen*/
+    /*Will contain: todo X) Exit - exit the application*/
+
+    /* todo This will be (" transactions.csv ") format */
+
+
+
+
+
+
+    //todo New screen ("Ledger") coming from Home Screen - All entries should show the newest entries first
+    //(May be a Class/Method) //Will need Scanner
+
+    /*Will contain: todo A) All - Display all entries*/
+    /*Will contain: todo D) Deposits - Display only the entries that are deposits into the account*/
+    /*Will contain: todo P) Payments - Display only the negative entries (or payments)*/
+    /*Will contain: todo R) Reports - A new screen that allows the user to run
+                     pre-defined reports or to run a custom search*/
+
+
+
+    //todo ("Reports") coming from Ledger Screen
+    //(May be a Class/Method) //Will need Scanner
+
+    /*Will contain: todo 1) Month To Date*/
+    /*Will contain: todo 2) Previous Month*/
+    /*Will contain: todo 3) Year To Date*/
+    /*Will contain: todo 4) Previous Year*/
+    /*Will contain: todo 5) Search by Vendor prompt the user for the
+                     vendor name and display all entries for that vendor*/
+    /*Will contain: todo 0) Back - go back to the Ledger page*/
+    /*Will contain: todo H) Home - go back to the home page*/
 }
--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -1,4 +1,41 @@
 package com.pluralsight;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.*;
 
+//I looked at the SearchInventory repository on GitHub to guide me on this!
 public class Transactions {
+    private LocalDate paymentDate;
+    private LocalTime paymentTime;
+    private String description;
+    private String vendor;
+    private Double amount;
+
+    public Transactions(LocalDate paymentDate,LocalTime paymentTime,String description,String vendor,Double amount){
+        this.paymentDate = paymentDate;
+        this.paymentTime = paymentTime;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+    public LocalDate getPaymentDate(){
+        return paymentDate;
+    }
+    public LocalTime getPaymentTime(){
+        return paymentTime;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public String getVendor(){
+        return vendor;
+    }
+    public Double getAmount(){
+        return amount;
+    }
+    @Override
+    public String toString(){
+        //I found a simpler way to write this instead of using something like: ("#%-10d %-18s %10.2f", id,name,price);
+        return paymentDate + "|" + paymentTime + "|" + description + "|" + vendor + "|" + amount;
+    }
 }
